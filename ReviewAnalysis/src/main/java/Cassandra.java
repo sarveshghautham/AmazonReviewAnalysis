082b43803a5e05346216be7709c5f88905d847c7
sarghau <sarvesh.kaladhar@gmail.com>
2014-11-08 23:46:14 -0500

    First Commit ============
    - Mapper reads the input file and takes the product id and product name
    - The product id and product name are inserted into a Cassandra DB
    - No reducer
    
    Note
    ====
    
    - Some modifications needed

---
+++ ReviewAnalysis/src/main/java/Cassandra.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+public class Cassandra {
+	private Cluster cluster;
+	
+	public Cassandra () {
+		connect("172.16.154.131");
+	}
+	
+	protected void finalize() {
+		close();
+	}
+	
+	public void connect(String node) {
+	   cluster = Cluster.builder()
+	         .addContactPoint(node).withPort(9160).build();
+	   Metadata metadata = cluster.getMetadata();
+	   System.out.printf("Connected to cluster: %s\n", 
+	         metadata.getClusterName());
+	   for ( Host host : metadata.getAllHosts() ) {
+	      System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
+	         host.getDatacenter(), host.getAddress(), host.getRack());
+	   }	   
+	}
+	
+	public void insertData (String query) {
+		Session session = cluster.connect();
+		System.out.println(query);
+		session.execute(query);      
+	}
+	
+	public ArrayList<Integer> selectQuery (String matchTag) {
+		Session session = cluster.connect();
+		String query = "SELECT * FROM ks_hashscore.hashscore where match_tag='"+matchTag+"' ORDER BY end_time DESC LIMIT 1";
+		ResultSet result = session.execute(query);
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		
+		for (Row row: result) {
+			temp.add(row.getInt("fours_count"));
+			temp.add(row.getInt("sixers_count"));
+			temp.add(row.getInt("wickets_count"));
+		}
+		return temp;
+	}
+	
+	public void close () {
+		cluster.close();
+	}
+	
+//	public static void main (String []args) {
+//		Cassandra casObj = new Cassandra();
+//		
+//		String query_try = "INSERT INTO pds_ks.id_to_name (product_id, product_name) VALUES ('B000FTPOMK', '14k Yellow Gold Butterfly Pendant, 16')";
+//		String query_try = "UPDATE pds_ks.id_to_name SET product_name='14k' WHERE product_id='B000FTPOMK'";
+//		String updateQuery = "UPDATE pds_ks.id_to_name "
+//				+ "SET product_name ='"+product_name+"' "
+//						+ "WHERE product_id='"+product_id+"'";
+//		casObj.insertData(updateQuery);
+//		
+//		String product_id = "B000FTPOMK";
+//		String product_name = "'14k Yellow Gold Butterfly Pendant, 16";
+//		
+//		String updateQuery = "UPDATE pds_ks.id_to_name "
+//		+ "SET product_name ='"+product_name+"' "
+//				+ "WHERE product_id='"+product_id+"'";
+//
+//		casObj.insertData(query_try);
+//		
+//	}
+	
+}
---
+++ ReviewAnalysis/src/main/java/ProductIdToName.java
@@ -0,0 +1,96 @@
+import java.io.IOException;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.lib.NullOutputFormat;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ProductIdToName {
+
+	public static class Map extends
+			Mapper<LongWritable, Text, NullWritable, NullWritable> {
+
+		private Cassandra casObj = new Cassandra();
+		private String product_id = "";
+		
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+
+			String line = value.toString();
+			String []tokens = line.split(" ");
+			
+			for (int i = 0; i < tokens.length; i++) {
+				String empty = "";
+				if (tokens[i].equals("product/productId:")) {
+					product_id = tokens[i+1];
+					String query = "INSERT INTO pds_ks.id_to_name (product_id, product_name) "
+							+ "VALUES ('"+tokens[i+1]+"', '"+empty+"')";
+					System.out.println(query);
+					casObj.insertData(query);
+				}
+				else if (tokens[i].equals("product/title:")) {
+					
+					String product_name = StringEscapeUtils.escapeSql(ConvertArrayToString(tokens));
+					String updateQuery = "UPDATE pds_ks.id_to_name "
+							+ "SET product_name ='"+product_name+"' "
+									+ "WHERE product_id='"+product_id+"'";
+					
+					casObj.insertData(updateQuery);
+					product_id = "";
+				}
+			}
+		}
+		
+		public String ConvertArrayToString (String []tokens) {
+			
+			StringBuilder sb = new StringBuilder();
+			for (int i = 1; i < tokens.length; i++) {
+				sb.append(tokens[i]);
+				if (i != tokens.length-1)
+					sb.append(" ");
+			}
+			return sb.toString();
+		}
+	}
+
+//	public static class Reduce extends
+//			Reducer<Text, IntWritable, Text, IntWritable> {
+//
+//		public void reduce(Text key, Iterable<IntWritable> values,
+//				Context context) throws IOException, InterruptedException {
+//			int sum = 0;
+//			for (IntWritable val : values) {
+//				sum += val.get();
+//			}
+//			context.write(key, new IntWritable(sum));
+//		}
+//	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "wordcount");
+		job.setJarByClass(WordCount.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(Map.class);
+		//job.setReducerClass(Reduce.class);
+		job.setNumReduceTasks(0);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputKeyClass(NullOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.waitForCompletion(true);
+	}
+
+}
