775d603abced30a1f95d3a59e5a932f89881eafc
sudharsannr <sudharsannr1@gmail.com>
2014-12-06 23:43:59 -0500

    Cassandra queries processing

--- ReviewAnalysis/src/main/java/Cassandra.java
+++ ReviewAnalysis/src/main/java/Cassandra.java
@@ -1,4 +1,7 @@
-import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
@@ -6,85 +9,156 @@ import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 
-public class Cassandra {
-	private Cluster cluster;
+public class Cassandra
+{
+	private static Cluster cluster;
+	private static Session session;
 	private static Cassandra dbObj = null;
-	
-	private Cassandra () {
-		
+	private static String ipAddr = null;
+	private static String keySpace = null;
+	private static String tableName = null;
+	private static int port = 0;
+	private static Properties prop = null;
+
+	private Cassandra()
+	{
 	}
-	public static Cassandra getInstance() {
-		if(dbObj == null){	
+
+	public static Cassandra getInstance()
+	{
+		if (dbObj == null)
 			dbObj = new Cassandra();
-		}
-		dbObj.connect("127.0.0.1");
-		
+		ipAddr = prop.getProperty("address");
+		port = Integer.parseInt(prop.getProperty("port"));
+		keySpace = prop.getProperty("keyspace");
+		tableName = prop.getProperty("table");
+		System.out.println(ipAddr + " " + port + " " + keySpace +  " " + tableName);
+		cluster = Cluster.builder().addContactPoint(ipAddr).withPort(port)
+				.build();
+		Metadata metadata = cluster.getMetadata();
+		System.out.printf("Connected to cluster: %s\n",
+				metadata.getClusterName());
+		for (Host host : metadata.getAllHosts())
+			System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
+					host.getDatacenter(), host.getAddress(), host.getRack());
+		session = cluster.connect();
 		return dbObj;
 	}
-	protected void finalize() {
+
+	private static void loadProperties()
+	{
+		prop = new Properties();
+		try
+		{
+			prop.load(new FileInputStream(
+					"src/main/resources/Cassandra.properties"));
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	private void dropKS()
+	{
+		String dropQuery = "DROP KEYSPACE IF EXISTS " + keySpace;
+		System.out.println(dropQuery);
+		session.execute(dropQuery);
+	}
+
+	private void createKS()
+	{
+		dropKS();
+		String createQuery = "CREATE KEYSPACE "
+				+ keySpace
+				+ " WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }";
+		System.out.println(createQuery);
+		session.execute(createQuery);
+	}
+
+	private void dropTable()
+	{
+		String dropQuery = "DROP TABLE IF EXISTS " + keySpace + ".test";
+		System.out.println(dropQuery);
+		session.execute(dropQuery);
+	}
+
+	private void createTable()
+	{
+		dropTable();
+		String createQuery = "CREATE TABLE " + keySpace
+				+ ".test (product_id varchar PRIMARY KEY,product_name varchar)";
+		System.out.println(createQuery);
+		session.execute(createQuery);
+	}
+
+	public void addProduct(String productIdentifier, String productName,
+			double posPercentage, String tags)
+	{
+		// INSERT INTO pds_ks.reviews (product_id, product_name,
+		// positive_percentage, top_k_words) VALUES ('B0000AOXLY',' 14k White
+		// Gold Freshwater Cultured Pearl 6.0-6.5mm Pendant, 18"
+		// ',100.0,'lovely:3||delicate:2||yellow:2||afraid:1||almost:1||also:1||certain:1||certainly:1||classy:1||eventually:1||great:1||little:1||purely:1||quite:1||simple:1||small:1||then:1||too:1||traditional:1||well:1||white:1||')
+		Statement statement = QueryBuilder.insertInto(keySpace, tableName)
+				.value("PRODUCT_ID", productIdentifier)
+				.value("PRODUCT_NAME", productName)
+				.value("POSITIVE_PERCENTAGE", posPercentage)
+				.value("TOP_K_WORDS", tags);
+		System.out.println(statement.toString());
+		session.execute(statement);
+	}
+
+	protected void finalize()
+	{
 		close();
 	}
-	
-	public void connect(String node) {
-	   cluster = Cluster.builder()
-	         .addContactPoint(node).withPort(9042).
-	         build();
-	   
-	   
-	   Metadata metadata = cluster.getMetadata();
-	   System.out.printf("Connected to cluster: %s\n", 
-	         metadata.getClusterName());
-	   for ( Host host : metadata.getAllHosts() ) {
-	      System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
-	         host.getDatacenter(), host.getAddress(), host.getRack());
-	   }	   
-	}
-	
-	public void insertData (String query) {
-		Session session = cluster.connect();
-		System.out.println(query);
-		session.execute(query);      
-	}
-	
-	public ArrayList<Integer> selectQuery (String matchTag) {
-		Session session = cluster.connect();
-		String query = "SELECT * FROM ks_hashscore.hashscore where match_tag='"+matchTag+"' ORDER BY end_time DESC LIMIT 1";
-		ResultSet result = session.execute(query);
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		
-		for (Row row: result) {
-			temp.add(row.getInt("fours_count"));
-			temp.add(row.getInt("sixers_count"));
-			temp.add(row.getInt("wickets_count"));
+
+	public String selectProduct(String productId)
+	{
+		/*
+		 * String query =
+		 * "SELECT positive_percentage, top_k_words FROM pds_ks.reviews where product_id='"
+		 * + productId + "'";
+		 */
+		Statement statement = QueryBuilder.select().all()
+				.from(keySpace, tableName).where(QueryBuilder.eq("PRODUCT_ID", productId));
+		if(null == statement)
+			System.out.println("Statement is null");
+		if(null == session)
+			System.out.println("Session is null");
+		ResultSet result = session.execute(statement);
+		StringBuilder results = new StringBuilder();
+		for (Row row : result)
+		{
+			Double d = row.getDouble("positive_percentage");
+			results.append(d.toString()).append("->")
+					.append(row.getString("top_k_words"));
 		}
-		return temp;
+		return results.toString();
 	}
-	
-	public void close () {
+
+	public void close()
+	{
+		session.close();
 		cluster.close();
 	}
-	
-//	public static void main (String []args) {
-//		Cassandra casObj = new Cassandra();
-//		
-//		String query_try = "INSERT INTO pds_ks.id_to_name (product_id, product_name) VALUES ('B000FTPOMK', '14k Yellow Gold Butterfly Pendant, 16')";
-//		String query_try = "UPDATE pds_ks.id_to_name SET product_name='14k' WHERE product_id='B000FTPOMK'";
-//		String updateQuery = "UPDATE pds_ks.id_to_name "
-//				+ "SET product_name ='"+product_name+"' "
-//						+ "WHERE product_id='"+product_id+"'";
-//		casObj.insertData(updateQuery);
-//		
-//		String product_id = "B000FTPOMK";
-//		String product_name = "'14k Yellow Gold Butterfly Pendant, 16";
-//		
-//		String updateQuery = "UPDATE pds_ks.id_to_name "
-//		+ "SET product_name ='"+product_name+"' "
-//				+ "WHERE product_id='"+product_id+"'";
-//
-//		casObj.insertData(query_try);
-		
-//	}
-	
+
+	public static void main(String[] args)
+	{
+		loadProperties();
+		Cassandra casObj = Cassandra.getInstance();
+		casObj.addProduct("xxx", "yyy", 10, "abc");
+		String data = casObj.selectProduct("xxx");
+		System.out.println("Data returned: " + data);
+		casObj.close();
+
+	}
 }
